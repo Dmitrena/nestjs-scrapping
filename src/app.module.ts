3ae1fe736d8ce3f964a2d0087480b1016dd6ea74
Dima <dziurziadmytro@gmail.com>
2024-12-23 17:51:07 +0200

    implement parsing products data from amazon

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AmazonModule } from './amazon/amazon.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AmazonModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
